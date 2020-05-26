dede2f05b98021a72cfbf49f979c8a2e249cd003
phonglk <phong.lekhoi@gmail.com>
2020-05-26 12:43:51 +0800

    add pre-install hooks

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+PKG="$(basename $1)"
+echo "Installing $PKG"
+
+if test -f "./$PKG/pre-install"; then
+  echo "Running pre-install script"
+  ./$PKG/pre-install
+fi
+
+if ! [ -x "$(command -v stow)" ]; then
+  echo "stow is not installed" >&2
+  exit 1
+fi
+
+echo Linking
+stow $PKG
+
+if test -f "./$PKG/post-install"; then
+  echo "Running post-install script"
+  ./$PKG/post-install
+fi
+
